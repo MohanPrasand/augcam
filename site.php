7f1eed79f039f5a6b9f921744c04f5fd7a1bdbde
Mohan Prasand Ramakrishnan <mohanprasand6@gmail.com>
2023-06-06 22:31:03 +0530

    Initial commit

---
+++ site.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <?php
+        null;
+        echo "<h1>Mohan Prasand</h1>";
+        echo "<hr>";
+        echo "<p>This is the php site I am doing at midnight 1:17</p>";
+        $myName="Mohan Prasand R";
+        $myAge=18;
+
+       ?>
+  </body>
+</html>
